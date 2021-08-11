bf2b4b6252c6616872b59e5f83dd3feaa1f2f498
Moonsik Park <moonsik.park@gmail.com>
2021-08-12 07:33:28 +0900

    Remove unnecessary whitespace.

--- prekernel/main.c
+++ prekernel/main.c
@@ -2,11 +2,11 @@
 
 void main()
 {
-    print_string(0, 5, "Prekernel started." );
+    print_string(0, 5, "Prekernel started.");
     while(1);
 }
 
-void print_string( int posX, int posY, const char *string)
+void print_string(int posX, int posY, const char *string)
 {
     screen_t *screen = (screen_t *) 0xB8000;
     
