7bc2b2acb8f078afb08e807ae3a2cdc5ccf9cb66
Moonsik Park <moonsik.park@gmail.com>
2021-08-12 17:47:25 +0900

    fix inb call.

--- prekernel/common.c
+++ prekernel/common.c
@@ -9,6 +9,8 @@ void outb(uint16_t port, uint8_t value)
 uint8_t inb(uint16_t port)
 {
     uint8_t ret;
-    __asm__ __volatile__("inb %1, %0" ::"=a"(ret), "dN"(port));
+    __asm__ __volatile__("inb %1, %0"
+                         : "=a"(ret)
+                         : "dN"(port));
     return ret;
 }
