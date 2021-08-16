c0cc323c7cea1b1fbd0d210ec74ff0c4b161477a
Moonsik Park <moonsik.park@gmail.com>
2021-08-16 14:29:44 +0900

    Add stdbool.h-like bool macros.

--- kernel/types.h
+++ kernel/types.h
@@ -6,4 +6,10 @@ typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long uint64_t;
 
+#define bool _Bool
+#define true 1
+#define false 0
+
+#define __bool_true_false_are_defined 1
+
 #endif /*__TYPES_H__*/
